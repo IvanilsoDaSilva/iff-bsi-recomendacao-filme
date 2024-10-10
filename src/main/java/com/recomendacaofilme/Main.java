63e8b74672145dfa38f8ca6b570618bb80e5c74a
Ivanilso da Silva Conceição Filho <ivanilso.silva@g8solucoes.com.br>
2024-10-10 17:28:11 -0300

    FEAT: Avanços no arquivo fcl

--- src/main/java/com/recomendacaofilme/Main.java
+++ src/main/java/com/recomendacaofilme/Main.java
@@ -1,7 +1,37 @@
 package main.java.com.recomendacaofilme;
 
+import java.nio.file.Paths;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
 public class Main {
-	public static void main(String[] args) {
-		
-	}
+    public static void main(String[] args) {
+        // Define o caminho do arquivo FCL
+        String fileName = Paths.get("src/main/java/com/recomendacaofilme/recomendacaofilme.fcl").toAbsolutePath().toString();
+
+        // Carrega o arquivo FCL
+        FIS fis = FIS.load(fileName, true);
+
+        // Verifica se houve erro ao carregar
+        if (fis == null) {
+            System.err.println("Não foi possível carregar o arquivo: '" + fileName + "'");
+            return;
+        }
+
+        // Define as variáveis de entrada
+        fis.setVariable("avaliacao", 3);  // Define a avaliação
+        fis.setVariable("duracao", 20);    // Define a duração
+        fis.setVariable("popularidade", 25); // Define a popularidade
+
+        // Avalia
+        fis.evaluate();
+
+        // Obtém e mostra a variável de saída
+        Variable tip = fis.getVariable("recomendacao");
+        JFuzzyChart.get().chart(tip, tip.getDefuzzifier(), true);
+
+        // Imprime o conjunto de regras
+        System.out.println(fis);
+    }
 }
